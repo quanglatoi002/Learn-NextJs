3fdaae87639ae7a85a2da059848c118d952270e7
quang <quanglatoi002@gmail.com>
2023-05-24 14:54:30 +0700

    Automatic Static Optimization

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,19 @@
-import React from "react";
+import { useRouter } from 'next/router';
+import React from 'react';
 
-type Props = {};
+export interface AboutPageProps {}
 
-const about = (props: Props) => {
-    return <div>about</div>;
-};
-
-export default about;
+// lúc đầu kết quả trả về bên phía clients
+// About query {} sẽ là 1 object rỗng
+// router.query empty when pre-rendering, update after hydration và output đầu ra là HTML
+export default function AboutPage(props: AboutPageProps) {
+  const router = useRouter();
+  console.log('About query', router.query);
+  return <div>about</div>;
+}
+//Sau khi use getServerSideProps output đầu ra là file JS
+export async function getServerSideProps() {
+  return {
+    props: { message: `Next.js is awesome` }, // will be passed to the page component as props
+  };
+}
