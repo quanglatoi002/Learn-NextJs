793356e2e08f48e9e99a7295d5d62473202da3da
quang <quanglatoi002@gmail.com>
2023-05-24 17:35:06 +0700

    SSG

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,9 +1,31 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next';
 import React from 'react';
 
-type Props = {};
+export interface Props {
+  post: any[];
+}
 
-const index = (props: Props) => {
-  return <div>index posts</div>;
+export default function PostListPage({ post }: Props) {
+  return (
+    <div>
+      index posts
+      <ul>
+        {post.map((item) => (
+          <li key={item.id}>{item.body}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps<Props> = async (context: GetStaticPropsContext) => {
+  console.log('static props');
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await response.json();
+  console.log(data);
+  return {
+    props: {
+      post: data,
+    },
+  };
 };
-
-export default index;
