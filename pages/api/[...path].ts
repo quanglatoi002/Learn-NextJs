e4d6e8ecf1db43698e5ab1466d03733341d0e889
quang <quanglatoi002@gmail.com>
2023-06-05 21:23:18 +0700

    handlelogin for the create authToken and forward HTTP-ONLY...

--- pages/api/[...path].ts
+++ pages/api/[...path].ts
@@ -2,7 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 //Chức năng chính của httpProxy là chuyển tiếp yêu cầu HTTP từ 1 máy khách đến 1 máy chủ khác.
 import httpProxy from 'http-proxy';
-
+import Cookies from 'cookies';
 // type Data = {
 //   name: string
 // }
@@ -17,15 +17,29 @@ export const config = {
 const proxy = httpProxy.createProxyServer();
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<any>) {
-    //ko gửi cookie tới phía server
-    //api/students
-    //https://js-post-api.herokuapp.com/api/sutdents
-    req.headers.cookie = '';
-    proxy.web(req, res, {
-        //.env.local chỉ được định nghĩa cho biến môi trường phát triển (run dev)
-        target: process.env.API_URL,
-        changeOrigin: true,
-        selfHandleResponse: false,
+    return new Promise((resolve) => {
+        ////convert cookies to header authorization
+        const cookies = new Cookies(req, res);
+        const accessToken = cookies.get('access_token');
+        if (accessToken) {
+            req.headers.Authorization = `Bearer ${accessToken}`;
+        }
+        //ko gửi cookie tới phía server
+        //api/students
+        //https://js-post-api.herokuapp.com/api/sutdents
+        req.headers.cookie = '';
+        //Nếu như bạn có nhu cầu giao tiếp với 1 API bên ngoài hoặc 1 máy chủ khác trong quá trình phát triển. Có thể dùng "proxy.web" để chuyển hướng các yêu cầu HTTP của ứng dụng từ máy chủ phát triển tới 1 máy chủ khác
+        proxy.web(req, res, {
+            //.env.local chỉ được định nghĩa cho biến môi trường phát triển (run dev)
+            target: process.env.API_URL,
+            changeOrigin: true,
+            selfHandleResponse: false, // điều này có nghĩa là Next.js sẽ tiếp tục xử lý phản hồi từ máy chủ phía Next.js và trả kết quả cho trình duyệt mà ko yêu cầu ứng dụng của bạn thực hiện xử lý phản hồi.
+        });
+        //once chỉ bắt sự kiện 1 lần duy nhất
+        proxy.once('proxyRes', () => {
+            // res.status(200).json({ name: 'John Doe' });
+            resolve(true);
+        });
+        // res.status(200).json({ name: 'John Doe' });
     });
-    // res.status(200).json({ name: 'John Doe' });
 }
---
+++ pages/api/login.ts
@@ -0,0 +1,60 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+//Chức năng chính của httpProxy là chuyển tiếp yêu cầu HTTP từ 1 máy khách đến 1 máy chủ khác.
+import httpProxy, { ProxyReqCallback } from 'http-proxy';
+import Cookies from 'cookies';
+
+type Data = {
+    message: string;
+};
+
+// để có thể tự xử lý nội dung yêu cầu trong hàm xử lý của router(ví dụ bên postman)
+export const config = {
+    api: {
+        //bodyParser được sử dụng để phân tích và xử lý nội dung yêu cầu HTTP như dữ liệu form hoặc JSON
+        bodyParser: false,
+    },
+};
+const proxy = httpProxy.createProxyServer();
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+    if (req.method !== 'POST') {
+        res.status(404).json({ message: 'method not supported' });
+    }
+
+    return new Promise((resolve) => {
+        //ko gửi cookie tới phía server
+        req.headers.cookie = '';
+        const handleLoginResponse: ProxyReqCallback = (proxyRes, req, res) => {
+            let body = '';
+            proxyRes.on('data', function (chunk) {
+                body += chunk;
+            });
+            proxyRes.on('end', function () {
+                try {
+                    const { accessToken, expiredAt } = JSON.parse(body);
+                    //convert token to cookies
+                    const cookies = new Cookies(req, res, {
+                        secure: process.env.NODE_ENV !== 'development',
+                    });
+                    cookies.set('access_token', accessToken, {
+                        httpOnly: true,
+                        sameSite: 'lax',
+                        expires: new Date(expiredAt),
+                    });
+                    (res as NextApiResponse).status(200).json({ message: 'login successful' });
+                } catch (error) {
+                    (res as NextApiResponse).status(500).json({ message: 'something went wrong' });
+                }
+                resolve(true);
+            });
+        };
+        // sự kiện proxyRes, khi client req forward the "target" server and the "target" server send back a res, lúc này "proxyRes" được kích hoạt
+        proxy.once('proxyRes', handleLoginResponse);
+        proxy.web(req, res, {
+            target: process.env.API_URL,
+            changeOrigin: true,
+            selfHandleResponse: true,
+        });
+    });
+}
