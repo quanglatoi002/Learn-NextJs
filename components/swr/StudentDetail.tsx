14f135deceb320d8588e88d83423a968e184ab2d
quang <quanglatoi002@gmail.com>
2023-06-07 16:02:34 +0700

    update SWR

--- components/swr/StudentDetail.tsx
+++ components/swr/StudentDetail.tsx
@@ -4,7 +4,22 @@ export interface StudentDetailProps {
     studentId: string;
 }
 
+const MS_PER_HOUR = 60 * 60 * 1000;
+
 export function StudentDetail({ studentId }: StudentDetailProps) {
-    const { data, error, mutate, isValidating } = useSWR(`/students/${studentId}`);
-    return <div>Name: {data?.name || '--'}</div>;
+    const { data, error, mutate, isValidating } = useSWR(`/students/${studentId}`, {
+        revalidateOnFocus: false,
+        // dedupingInterval: MS_PER_HOUR,
+        dedupingInterval: 2000,
+    });
+    function handleMutateClick() {
+        //mutate được dùng để cập nhật dữ liệu trong cache và gửi yêu cầu cập nhật dữ liệu mới từ máy chủ
+        mutate({ name: 'frontend' }, false);
+    }
+    return (
+        <div>
+            Name: {data?.name || '--'}
+            <button onClick={handleMutateClick}>mutate</button>
+        </div>
+    );
 }
