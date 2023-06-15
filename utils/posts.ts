0af0e3cc31f4fe77cddbfb7842f0b56454ad893d
quang <quanglatoi002@gmail.com>
2023-06-15 17:46:41 +0700

    update coding

--- utils/posts.ts
+++ utils/posts.ts
@@ -8,8 +8,14 @@ export async function getPostList() {
     console.log(process.cwd());
     //real all markdown files
     //method này trả về 1 mảng chứa tên của các tệp và thư mục
+    //kq khi readdirSync '[ 'blog1.md', 'blog2.md', 'blog3.md' ]'
     const fileNameList = fs.readdirSync(BLOG_FOLDER);
+    for (const fileName of fileNameList) {
+        // kq sau khi nối fileName với BLOG_FOLDER là /Users/phamvanquang/Workspace/nextjs/learn-nextjs/blog/blog1.md
+        const filePath = path.join(BLOG_FOLDER, fileName);
+        //đọc kq bên trong file có gì
+        const fileContent = fs.readFileSync(filePath, 'utf8');
+    }
 
-    console.log(fileNameList);
     return [];
 }
